8c389f3290ab0d8266ebf5be2f329b77fbfa7809
YEYINXU <yyx19931007@outlook.com>
2022-12-03 19:15:50 +0800

    Dynamically create and delete controls

--- Demo/Form1.Designer.cs
+++ Demo/Form1.Designer.cs
@@ -28,12 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btn_CreateCons = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_CreateCons
+            // 
+            this.btn_CreateCons.Location = new System.Drawing.Point(313, 59);
+            this.btn_CreateCons.Name = "btn_CreateCons";
+            this.btn_CreateCons.Size = new System.Drawing.Size(75, 23);
+            this.btn_CreateCons.TabIndex = 0;
+            this.btn_CreateCons.Text = "Create";
+            this.btn_CreateCons.UseVisualStyleBackColor = true;
+            this.btn_CreateCons.Click += new System.EventHandler(this.btn_CreateCons_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_CreateCons);
+            this.KeyPreview = true;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Button btn_CreateCons;
     }
 }
--- Demo/Form1.cs
+++ Demo/Form1.cs
@@ -2,9 +2,40 @@ namespace Demo
 {
     public partial class Form1 : Form
     {
+        string preFocusPic;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btn_CreateCons_Click(object sender, EventArgs e)
+        {
+            int picCount = 3;
+            int interval = 10;
+            for (int i = 0; i < picCount; i++)
+            {
+                PictureBox newPic = new();
+                newPic.Name = "pic_" + i;
+                newPic.Location = new Point(0, (newPic.Size.Height + interval) * i);
+                newPic.BackColor = Color.Black;
+                newPic.Parent = this;
+                newPic.Click += new EventHandler(picClickMark);
+            }
+            btn_CreateCons.Enabled = false;
+        }
+
+        void picClickMark(object sender, EventArgs e)
+        {
+            preFocusPic = (sender as Control).Name;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Delete && !string.IsNullOrEmpty(preFocusPic))
+            {
+                try { (this.Controls.Find(preFocusPic, false)[0]).Dispose(); }
+                catch { }
+            }
         }
     }
 }
